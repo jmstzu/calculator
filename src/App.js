52987a7a1de2f4dc1eb7fa3eda9cd2dc959070cc
Yachii19 <johnreinvinuya19@gmail.com>
2024-09-26 11:52:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function Key({label, clickHandler}) {
+  return(
+    <button onClick={clickHandler}>
+      {label}
+    </button>
+  )
+}
+
+function Display(label) {
+  return(
+    <div className="Display">
+      CalcHub
+    </div>
+  )
+}
+
 function App() {
+
+  const genericClickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    alert(value)
+    console.log(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="CalcContainer">
+        <div className="DispContainer">
+          <Display />
+       </div>
+       <div className="ButtonsContainer">
+        <Key label={7} clickHandler={genericClickHandler}/>
+        <Key label={8}  clickHandler={genericClickHandler}/>
+        <Key label={9}  clickHandler={genericClickHandler}/>
+        <Key label={"÷"}  clickHandler={genericClickHandler}/>
+        <Key label={4}  clickHandler={genericClickHandler}/>
+        <Key label={5}  clickHandler={genericClickHandler}/>
+        <Key label={6}  clickHandler={genericClickHandler}/>
+        <Key label={"*"}  clickHandler={genericClickHandler}/>
+        <Key label={3}  clickHandler={genericClickHandler}/>
+        <Key label={2}  clickHandler={genericClickHandler}/>
+        <Key label={1}  clickHandler={genericClickHandler}/>
+        <Key label={"-"}  clickHandler={genericClickHandler}/>
+        <Key label={"c"}  clickHandler={genericClickHandler}/>
+        <Key label={1}  clickHandler={genericClickHandler}/>
+        <Key label={"="}  clickHandler={genericClickHandler}/>
+        <Key label={"+"}  clickHandler={genericClickHandler}/>
+      </div>
+      </div>
     </div>
   );
 }
